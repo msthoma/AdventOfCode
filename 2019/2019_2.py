a00a3752c27a3ec0b1d575ac8d419e9f4c93b812
marios <msthoma@yahoo.com>
2019-12-04 14:58:51 +0200

    convert to using util functions

--- 2019/2019_2.py
+++ 2019/2019_2.py
@@ -1,3 +1,6 @@
+from utils import utils
+
+
 def computer(intcode, pos=0):
     start_pos = pos * 4
     opcode = intcode[start_pos]
@@ -23,28 +26,23 @@ def run_program(intcode, noun, verb):
 
 
 def combinations(start, stop):
-    # comb = []
-    # for i in range(start, stop + 1):
-    #     for j in range(start, stop + 1):
-    #         comb.append([i, j])
     return ([j, i] for j in range(start, stop + 1) for i in range(start, stop + 1))
 
 
 def main():
-    with open("day2_input.txt", "r") as f:
+    day = utils.get_day_name()
+    with open(f"{day}.txt", "r") as f:
         input_data = f.read().split(sep=",")
 
     input_data = [int(i) for i in input_data]
 
-    print("day 2, part 1:", run_program(input_data.copy(), 12, 2))
+    utils.print_res(day, 1, run_program(input_data.copy(), 12, 2))
 
     for cmb in combinations(0, 99):
         res = run_program(input_data.copy(), cmb[0], cmb[1])
         if res == 19690720:
-            print("day 2, part 2:", cmb, 100 * cmb[0] + cmb[1])
+            utils.print_res(day, 2, 100 * cmb[0] + cmb[1])
             break
-
-    # print(combinations(0, 99))
 
 
 if __name__ == '__main__':
