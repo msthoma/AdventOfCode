43c7302d6d9181fe857921212e2fe46092fbc34f
marios <msthoma@yahoo.com>
2019-12-04 14:47:58 +0200

    function to print results

--- utils/utils.py
+++ utils/utils.py
@@ -4,3 +4,7 @@ import sys
 
 def get_day_name():
     return os.path.splitext(os.path.basename(sys.argv[0]))[0]
+
+
+def print_part_result(day_name, part, res):
+    print(f"{day_name}_{part} answer:", res)
