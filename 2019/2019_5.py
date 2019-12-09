6dfd141f52c11b85a06b2a9f699833d6608ce5c4
marios <msthoma@yahoo.com>
2019-12-09 18:51:14 +0200

    implement modes for opcode 4

--- 2019/2019_5.py
+++ 2019/2019_5.py
@@ -3,7 +3,7 @@ from utils import utils
 
 def computer2(intcode, ip=0):
     opcode = intcode[ip]
-    # print("opcode", opcode)
+
     if opcode == 99:
         # terminate
         return intcode
@@ -12,21 +12,22 @@ def computer2(intcode, ip=0):
         opcode = int(opcode_str[-1])
         assert opcode in [1, 2, 3, 4]
 
-        if opcode == 3:
-            intcode[intcode[ip + 1]] = int(input("Enter integer input: "))
-            ip += 2
-        elif opcode == 4:
-            print("output:", intcode[intcode[ip + 1]])
-            ip += 2
-        else:
-            modes = [0] * 3
+        modes = [0] * 3
+
+        if len(opcode_str) != 1:
             for i, mode in enumerate(opcode_str[:-2][::-1]):
                 mode = int(mode)
                 assert mode in [0, 1]
                 modes[i] = mode
 
-            # print(modes)
-
+        if opcode == 3:
+            intcode[intcode[ip + 1]] = int(input("Enter integer input: "))
+            ip += 2
+        elif opcode == 4:
+            output = intcode[intcode[ip + 1]] if modes[0] == 0 else intcode[ip + 1]
+            print("output:", output)
+            ip += 2
+        else:
             p1 = intcode[intcode[ip + 1]] if modes[0] == 0 else intcode[ip + 1]
             p2 = intcode[intcode[ip + 2]] if modes[1] == 0 else intcode[ip + 2]
 
@@ -46,8 +47,7 @@ def main():
 
     input_data = [int(i) for i in input_data]
 
-    res = computer2(input_data)
-    print(res)
+    computer2(input_data)
 
 
 if __name__ == '__main__':
