dbc4de8fc15bd73c55fb4182eed626775b76d0b0
marios <msthoma@yahoo.com>
2019-12-15 19:47:27 +0200

    import new intcode

--- aoc2019/2019_7.py
+++ aoc2019/2019_7.py
@@ -1,10 +1,12 @@
 from utils.utils import day_name, input_fp, print_res
+from aoc2019.intcode import computer
 
 
 def main():
     day = day_name()
     with open(input_fp(day), "r") as f:
-        input_data = [int(i) for i in f.read().split(sep=",")]
+        intcode = [int(i) for i in f.read().split(sep=",")]
+    print(list(computer(intcode, 1)))
 
 
 if __name__ == '__main__':
