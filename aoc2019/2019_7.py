9e2568c0c8ba723f2e4823259ff1f6642f4a81f3
marios <msthoma@yahoo.com>
2019-12-15 20:39:04 +0200

    part 1 done

--- aoc2019/2019_7.py
+++ aoc2019/2019_7.py
@@ -1,12 +1,23 @@
-from utils.utils import day_name, input_fp, print_res
+from itertools import permutations
+
 from aoc2019.intcode import computer
+from utils.utils import day_name, input_fp, print_res
 
 
 def main():
     day = day_name()
     with open(input_fp(day), "r") as f:
         intcode = [int(i) for i in f.read().split(sep=",")]
-    print(list(computer(intcode, 1)))
+
+    comb_dict = {}
+    for comb in permutations(range(5)):
+        input_signal = 0
+        for phase in comb:
+            res = list(computer(intcode.copy(), [phase, input_signal]))
+            input_signal = res[-1]
+        comb_dict[comb] = input_signal
+
+    print_res(day, 1, max(comb_dict.values()))
 
 
 if __name__ == '__main__':
