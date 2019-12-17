698d82b1e24a494ac68c4c424635e54650b18be3
marios <msthoma@yahoo.com>
2019-12-17 12:18:39 +0200

    day 8 part 2

--- aoc2019/2019_8.py
+++ aoc2019/2019_8.py
@@ -1,5 +1,7 @@
-from utils.utils import day_name, input_fp, print_res
+import matplotlib.pylab as plt
 import numpy as np
+
+from utils.utils import day_name, input_fp, print_res
 
 
 def main():
@@ -7,11 +9,38 @@ def main():
     with open(input_fp(day), "r") as f:
         img = np.array([int(i) for i in f.read().strip()])
 
-    img_reshaped = np.split(img.reshape((-1, 25)), len(img) / (6 * 25))
+    # img dimensions
+    height = 6
+    width = 25
+
+    # part 1
+    img_reshaped = np.split(img.reshape((-1, width)), len(img) / (height * width))
     zero_counts = [np.count_nonzero(s == 0) for s in img_reshaped]
     min_zeros_layer = img_reshaped[zero_counts.index(min(zero_counts))]
 
     print_res(day, 1, np.count_nonzero(min_zeros_layer == 1) * np.count_nonzero(min_zeros_layer == 2))
+
+    # numpy option to print arrays entirely
+    np.set_printoptions(threshold=np.inf)
+
+    # part 2
+    img_reshaped = np.stack(img_reshaped, axis=2)
+
+    res = []
+    for i in range(height):
+        for j in range(width):
+            for v in img_reshaped[i, j, :]:
+                if v in [0, 1]:
+                    res.append(v)
+                    break
+
+    res = np.reshape(res, (height, width))
+    print_res(day, 2, res)
+
+    # plot image
+    plt.imshow(res, cmap="binary")
+    plt.savefig("2019_8_2_img.svg")
+    plt.show()
 
 
 if __name__ == '__main__':
