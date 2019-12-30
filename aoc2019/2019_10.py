5b4566bf21fccf116bd29ac646b2048a02b0b8cc
marios <msthoma@yahoo.com>
2019-12-30 15:06:46 +0200

    remove commented block

--- aoc2019/2019_10.py
+++ aoc2019/2019_10.py
@@ -31,40 +31,6 @@ def main():
 
         asteroid_los[tuple(station)] = len(in_los)
 
-        # a = np.array(a)
-        # # euclidean distances with other asteroids
-        # a_eucl = {}
-        # for b in asteroid_coord:
-        #     b = np.array(b)
-        #     if np.array_equal(a, b):
-        #         continue
-        #     eucl_d = norm(a - b)
-        #     # center at a (by b - a) and find angle of b from center - https://en.wikipedia.org/wiki/Atan2
-        #     angle = np.arctan2(*(b - a))
-        #     # ab = np.dot(a, b)
-        #     # normab = norm(a) * norm(b)
-        #     # line_angle = np.arccos((a @ b) / (norm(a) * norm(b)))
-        #     manhattan = list(np.absolute(a - b))
-        #     a_eucl[tuple(b)] = {"eucl": eucl_d, "angle": angle}
-        #
-        # a_eucl = {k: v for k, v in sorted(a_eucl.items(), key=lambda item: item[1]["eucl"])}
-        # print(a_eucl)
-        #
-        # angles = [v["angle"] for k, v in a_eucl.items()]
-        # print(sorted(angles))
-        # for asteroid, val in a_eucl.items():
-        #     pass
-
-        # asteroids in line of sight of a
-        # in_line = []
-        # blocked_lines = []
-        # ax, ay = a
-        # for bx, by in a_eucl:
-        #     if not in_line:
-        #         in_line.append([bx, by])
-        #         blocked_lines.append([bx, by])
-        # print(in_line)
-        # print(10 % 0)
     max_los = max(asteroid_los, key=asteroid_los.get)
 
     print_res(day, 1, asteroid_los[max_los])
