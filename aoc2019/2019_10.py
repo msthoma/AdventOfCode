0b7199c35ae29b5d59ac0461ff363f9e5ff41b97
marios <msthoma@yahoo.com>
2020-01-02 11:07:20 +0200

    part 2 answer

--- aoc2019/2019_10.py
+++ aoc2019/2019_10.py
@@ -13,13 +13,14 @@ def main():
 
     asteroid_coords = []
 
-    for y in range(asteroid_map.shape[0]):
-        for x in range(asteroid_map.shape[1]):
-            if asteroid_map[y, x] == 1:
+    for x in range(asteroid_map.shape[0]):
+        for y in range(asteroid_map.shape[1]):
+            if asteroid_map[x, y] == 1:
                 asteroid_coords.append([x, y])
 
-    asteroid_max_los = {}
-    asteroid_los = {}
+    # part 1
+    los_asteroid_counts = {}
+    los_asteroids_rel = {}
 
     for station in asteroid_coords:
         in_los = set()
@@ -30,12 +31,25 @@ def main():
                 gcd = abs(math.gcd(dx, dy))
                 in_los.add((dx // gcd, dy // gcd))
 
-        asteroid_los[tuple(station)] = in_los
-        asteroid_max_los[tuple(station)] = len(in_los)
+        los_asteroids_rel[tuple(station)] = in_los
+        los_asteroid_counts[tuple(station)] = len(in_los)
 
-    max_los = max(asteroid_max_los, key=asteroid_max_los.get)
+    station = max(los_asteroid_counts, key=los_asteroid_counts.get)
 
-    print_res(day, 1, asteroid_max_los[max_los])
+    print_res(day, 1, los_asteroid_counts[station])
+
+    # part 2
+    los_angles = {math.atan2(y, x): (x, y) for x, y in los_asteroids_rel[station]}
+
+    asteroid_200th = los_angles[sorted(los_angles.keys(), reverse=True)[200 - 1]]
+    dx, dy = asteroid_200th
+
+    x, y = station[0] + dx, station[1] + dy
+
+    while [x, y] not in asteroid_coords:
+        x, y = x + dx, y + dy
+
+    print_res(day, 2, y * 100 + x)
 
 
 if __name__ == '__main__':
