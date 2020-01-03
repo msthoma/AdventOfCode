1c57890d613a0048e84b558f1db6f890989bc8ff
marios <msthoma@yahoo.com>
2020-01-03 20:18:07 +0200

    part 1 finished

--- aoc2019/2019_11.py
+++ aoc2019/2019_11.py
@@ -1,5 +1,29 @@
 from aoc2019.intcode import computer
 from utils.utils import day_name, input_fp, print_res
+from collections import defaultdict
+
+d_list = list("LURD")
+d_dict = {k: v for k, v in zip(d_list, range(4))}
+
+
+def turn(position, direction, instruction):
+    x, y = position
+
+    if instruction == 0:
+        instruction = -1
+
+    new_dir = d_list[(d_dict[direction] + instruction) % len(d_list)]
+
+    if new_dir == "L":
+        x -= 1
+    elif new_dir == "U":
+        y += 1
+    elif new_dir == "R":
+        x += 1
+    elif new_dir == "D":
+        y -= 1
+
+    return (x, y), new_dir
 
 
 def main():
@@ -7,7 +31,39 @@ def main():
     with open(input_fp(day), "r") as f:
         intcode = [int(i) for i in f.read().split(sep=",")]
 
-    print(intcode)
+    # print(intcode)
+
+    robot_path = [(0, 0)]  # answer will be set of this
+    robot_direction = ["U"]
+    panel_color = defaultdict(int)
+
+    halt = False
+    ip = 0
+    while True:
+        current_panel = robot_path[-1]
+        current_color = panel_color[current_panel]
+        # get color
+        res = computer(intcode, [current_color], ip=ip, feedback=True, extend=True)
+        # check if finished
+        halt = res["halt"]
+        if halt:
+            break
+        # paint panel with color
+        panel_color[robot_path[-1]] = res["output"][0]
+        ip = res["ip"]
+
+        # get instruction
+        res = computer(intcode, [current_color], ip=ip, feedback=True)
+        instruction = res["output"][0]
+        # update ip
+        ip = res["ip"]
+
+        new_pos, new_dir = turn(current_panel, robot_direction[-1], instruction)
+
+        robot_path.append(new_pos)
+        robot_direction.append(new_dir)
+
+    print_res(day, 1, len(set(robot_path)))
 
 
 if __name__ == '__main__':
