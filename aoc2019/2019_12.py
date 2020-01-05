cefabc5ff08879841f2cdda9b0fbd4a92053e9ce
marios <msthoma@yahoo.com>
2020-01-05 21:21:04 +0200

    day 12 part 2 answer

--- aoc2019/2019_12.py
+++ aoc2019/2019_12.py
@@ -1,5 +1,8 @@
 import re
+from copy import deepcopy
 from itertools import combinations
+
+import numpy as np
 
 from utils.utils import day_name, input_fp, print_res
 
@@ -28,6 +31,7 @@ def main():
     with open(input_fp(day), "r") as f:
         data = [re.match(pattern, line).groups() for line in f.readlines()]
 
+    # part 1
     positions = {}
     velocities = {}
 
@@ -55,6 +59,34 @@ def main():
         total += sum(abs(p) for p in positions[moon].values()) * sum(abs(v) for v in velocities[moon].values())
 
     print_res(day, 1, total)
+
+    # part 2
+    # arrange dictionaries based on dimension
+    pos1 = {k: {} for k in dimensions}
+    vel1 = {k: {} for k in dimensions}
+    for i, dim in enumerate(pos1.keys()):
+        for j, moon in enumerate(moons):
+            pos1[dim][moon] = int(data[j][i])
+            vel1[dim][moon] = 0
+
+    # make copies to compare with original after each simulation
+    pos2 = deepcopy(pos1)
+    vel2 = deepcopy(vel1)
+
+    # simulate each dimension separately, until it reaches values identical to the original
+    steps_all = []
+    for dim in dimensions:
+        steps = 0
+        while True:
+            simulate(pos2[dim], vel2[dim])
+            steps += 1
+            if pos1[dim] == pos2[dim] and vel1[dim] == vel2[dim]:
+                steps_all.append(steps)
+                break
+
+    # answer is the least common multiple of steps for each dimension
+    lcm = np.lcm.reduce(steps_all)
+    print_res(day, 2, lcm)
 
 
 if __name__ == '__main__':
