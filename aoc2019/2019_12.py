87b40b440cf99f568033e9f79756647c6d1b6b48
marios <msthoma@yahoo.com>
2020-01-05 19:49:09 +0200

    day 12 part 1 answer

--- aoc2019/2019_12.py
+++ aoc2019/2019_12.py
@@ -1,7 +1,7 @@
 import re
 from itertools import combinations
 
-from utils.utils import day_name, input_fp
+from utils.utils import day_name, input_fp, print_res
 
 
 def main():
@@ -21,21 +21,26 @@ def main():
         positions[moon] = {"x": int(x), "y": int(y), "z": int(z)}
         velocities[moon] = {"x": 0, "y": 0, "z": 0}
 
-    for moon1, moon2 in combinations(positions.keys(), 2):
-        for dim in dimensions:
-            if positions[moon1][dim] < positions[moon2][dim]:
-                velocities[moon1][dim] += 1
-                velocities[moon2][dim] -= 1
-            elif positions[moon1][dim] > positions[moon2][dim]:
-                velocities[moon1][dim] -= 1
-                velocities[moon2][dim] += 1
+    for _ in range(1000):
+        for moon1, moon2 in combinations(positions.keys(), 2):
+            for dim in dimensions:
+                if positions[moon1][dim] < positions[moon2][dim]:
+                    velocities[moon1][dim] += 1
+                    velocities[moon2][dim] -= 1
+                elif positions[moon1][dim] > positions[moon2][dim]:
+                    velocities[moon1][dim] -= 1
+                    velocities[moon2][dim] += 1
 
+        for moon in moons:
+            for dim in dimensions:
+                positions[moon][dim] += velocities[moon][dim]
+
+    # calculate total energy
+    total = 0
     for moon in moons:
-        for dim in dimensions:
-            positions[moon][dim] += velocities[moon][dim]
+        total += sum(abs(p) for p in positions[moon].values()) * sum(abs(v) for v in velocities[moon].values())
 
-    print(velocities)
-    print(positions)
+    print_res(day, 1, total)
 
 
 if __name__ == '__main__':
