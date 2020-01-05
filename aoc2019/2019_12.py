d45e0f1149229788b1c91907a427b111de4a6b99
marios <msthoma@yahoo.com>
2020-01-05 19:33:44 +0200

    calculating gravity and velocities complete

--- aoc2019/2019_12.py
+++ aoc2019/2019_12.py
@@ -1,5 +1,7 @@
-from utils.utils import day_name, input_fp
 import re
+from itertools import combinations
+
+from utils.utils import day_name, input_fp
 
 
 def main():
@@ -10,11 +12,30 @@ def main():
     with open(input_fp(day), "r") as f:
         data = [re.match(pattern, line).groups() for line in f.readlines()]
 
-    moons = {}
+    dimensions = ["x", "y", "z"]
+    moons = ["Io", "Europa", "Ganymede", "Callisto"]
+    positions = {}
+    velocities = {}
 
-    for [x, y, z], moon in zip(data, ["Io", "Europa", "Ganymede", "Callisto"]):
-        moons[moon] = {"x": x, "y": y, "z": z}
-        print(moons[moon])
+    for [x, y, z], moon in zip(data, moons):
+        positions[moon] = {"x": int(x), "y": int(y), "z": int(z)}
+        velocities[moon] = {"x": 0, "y": 0, "z": 0}
+
+    for moon1, moon2 in combinations(positions.keys(), 2):
+        for dim in dimensions:
+            if positions[moon1][dim] < positions[moon2][dim]:
+                velocities[moon1][dim] += 1
+                velocities[moon2][dim] -= 1
+            elif positions[moon1][dim] > positions[moon2][dim]:
+                velocities[moon1][dim] -= 1
+                velocities[moon2][dim] += 1
+
+    for moon in moons:
+        for dim in dimensions:
+            positions[moon][dim] += velocities[moon][dim]
+
+    print(velocities)
+    print(positions)
 
 
 if __name__ == '__main__':
