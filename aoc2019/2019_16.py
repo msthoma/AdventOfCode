b0d12b6797e279cf9cd962571e04921418aed7e5
marios <msthoma@yahoo.com>
2020-01-14 20:31:34 +0200

    part 2 takes too long, must find new way of doing the calculations

--- aoc2019/2019_16.py
+++ aoc2019/2019_16.py
@@ -1,8 +1,9 @@
 import time
 
 import numpy as np
+from tqdm import tqdm
 
-from utils.utils import day_name, input_fp, print_res
+from utils.utils import day_name, input_fp
 
 
 def get_pattern(i, leng):
@@ -18,10 +19,13 @@ def run_fft_np(signal, times):
     leng = signal.size
     for _ in range(times):
         temp_data = []
+        t = tqdm(total=leng)
         for i in range(1, leng + 1):
             pat = np.array(get_pattern(i, leng))
             temp_data.append(abs(np.sum(signal * pat)) % 10)
+            t.update()
         signal = np.array(temp_data)
+        t.close()
     return signal
 
 
@@ -45,18 +49,27 @@ def main():
         data = [int(i) for i in f.read().strip()]
 
     # part 1
-    print_res(day, 1, "".join(str(i) for i in run_fft(data.copy(), 100)[:8]))
+    # print_res(day, 1, "".join(str(i) for i in run_fft(data.copy(), 100)[:8]))
 
     # part 1 try with numpy
     with open(input_fp(day), "r") as f:
         data_np = np.array([int(i) for i in f.read().strip()])
 
-    print_res(day, 1, "".join(str(i) for i in run_fft_np(data_np.copy(), 100)[:8]))
+    # print_res(day, 1, "".join(str(i) for i in run_fft_np(data_np.copy(), 100)[:8]))
 
     # data_11 = np.array(data)
 
-    # part 2
-    # offset = "".join(str(i) for i in data[:7])
+    # part 2 non-numpy
+    # data = [int(i) for i in "03036732577212944063491565474664"]
+    offset = int("".join(str(i) for i in data[:7]))
+    print(offset)
+    data = data * 10000
+    # out = run_fft(data.copy(), 100)
+    # print(out[offset:offset + 8])
+
+    out = run_fft_np(np.array(data.copy()), 100)
+    print(out[offset:offset + 8])
+
     # data_2 = data * 10000
     end = time.time()
     print(end - start)
