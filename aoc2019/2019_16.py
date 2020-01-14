58246dfa1256480b8951a84bb98ee65753908a7d
marios <msthoma@yahoo.com>
2020-01-14 19:59:45 +0200

    moved non-numpy way to function

--- aoc2019/2019_16.py
+++ aoc2019/2019_16.py
@@ -1,5 +1,6 @@
-import numpy as np
 import time
+
+import numpy as np
 
 from utils.utils import day_name, input_fp, print_res
 
@@ -13,7 +14,7 @@ def get_pattern(i, leng):
     return pat[1:leng + 1]
 
 
-def run_fft(signal, times):
+def run_fft_np(signal, times):
     leng = signal.size
     for _ in range(times):
         temp_data = []
@@ -21,6 +22,17 @@ def run_fft(signal, times):
             pat = np.array(get_pattern(i, leng))
             temp_data.append(abs(np.sum(signal * pat)) % 10)
         signal = np.array(temp_data)
+    return signal
+
+
+def run_fft(signal, times):
+    leng = len(signal)
+    for _ in range(times):
+        temp_data = []
+        for i in range(1, leng + 1):
+            pat = get_pattern(i, leng)
+            temp_data.append(abs(sum(si * pv for si, pv in zip(signal, pat))) % 10)
+        signal = temp_data
     return signal
 
 
@@ -33,21 +45,13 @@ def main():
         data = [int(i) for i in f.read().strip()]
 
     # part 1
-    data_1 = data.copy()
-    leng = len(data_1)
-    for _ in range(100):
-        temp_data = []
-        for i in range(1, leng + 1):
-            pat = get_pattern(i, leng)
-            temp_data.append(abs(sum(si * pv for si, pv in zip(data_1, pat))) % 10)
-        data_1 = temp_data
-
-    print_res(day, 1, "".join(str(i) for i in data_1[:8]))
+    print_res(day, 1, "".join(str(i) for i in run_fft(data.copy(), 100)[:8]))
 
     # part 1 try with numpy
     with open(input_fp(day), "r") as f:
         data_np = np.array([int(i) for i in f.read().strip()])
-    print_res(day, 1, "".join(str(i) for i in run_fft(data_np.copy(), 100)[:8]))
+
+    print_res(day, 1, "".join(str(i) for i in run_fft_np(data_np.copy(), 100)[:8]))
 
     # data_11 = np.array(data)
 
