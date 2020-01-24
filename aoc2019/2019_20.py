167b7342d0999bcce9bc4786beb091cd51bf63eb
marios <msthoma@yahoo.com>
2020-01-24 16:38:35 +0200

    account for recursive layers of part 2, also part 2 answer

--- aoc2019/2019_20.py
+++ aoc2019/2019_20.py
@@ -2,8 +2,13 @@ from collections import defaultdict, deque
 
 from utils.utils import day_name, input_fp, print_res
 
+in_key, out_key = "in", "out"
+
 
 def find_min_path(start, end, grid, portals, recursive=False):
+    """
+    Finds minimum path length to end of maze, using breadth-first search
+    """
     initial_layer = 0
     start, end = (start, initial_layer), (end, initial_layer)
     bfs = deque([start])
@@ -24,15 +29,22 @@ def find_min_path(start, end, grid, portals, recursive=False):
 def get_neighbours(pt, grid, portals, recursive=False):
     """
     Determines reachable neighbours from given point
+    Accounts for recursive layers of the maze, if they exist
     """
     current_pt, current_layer = pt
     neighbours = []
     # add neighbours via portals
     if current_pt in portals:
-        if recursive:  # multiple layers, for part 2
-            pass
-        else:  # only one layer, for part 1
-            neighbours.append(([*portals[current_pt]][0], current_layer))
+        portal_neighbour = [*portals[current_pt]][0]
+        neighbour_layer = current_layer
+
+        if recursive:  # case of multiple layers, for part 2
+            direction = 1 if portals[current_pt][portal_neighbour] == in_key else -1
+            if neighbour_layer + direction >= 0:
+                # if neighbour_layer < 0 it's a portal on the "outest" layer, so  destination is not valid
+                neighbours.append((portal_neighbour, neighbour_layer + direction))
+        else:
+            neighbours.append((portal_neighbour, neighbour_layer))
 
     # possible movements (diagonally is impossible)
     dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]
@@ -53,7 +65,6 @@ def main():
 
     # identify portals from input
     portal_pairs = defaultdict(dict)  # portal_pairs in form {"key": {(external pt): "out", (internal pt): "in"}}
-    inner_key, outer_key = "in", "out"
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j].isalpha():
@@ -61,33 +72,33 @@ def main():
                     if grid[i + 1][j].isalpha() and grid[i + 2][j] == ".":
                         key, pt = grid[i][j] + grid[i + 1][j], (i + 2, j)
                         if pt[0] == 2:  # outer top
-                            portal_pairs[key][pt] = outer_key
+                            portal_pairs[key][pt] = out_key
                         else:  # inner down
-                            portal_pairs[key][pt] = inner_key
+                            portal_pairs[key][pt] = in_key
 
                 if i - 2 >= 0:  # v up
                     if grid[i - 1][j].isalpha() and grid[i - 2][j] == ".":
                         key, pt = grid[i - 1][j] + grid[i][j], (i - 2, j)
                         if pt[0] == 28:  # inner top
-                            portal_pairs[key][pt] = inner_key
+                            portal_pairs[key][pt] = in_key
                         else:  # outer down
-                            portal_pairs[key][pt] = outer_key
+                            portal_pairs[key][pt] = out_key
 
                 if j + 2 < len(grid[0]):  # h right
                     if grid[i][j + 1].isalpha() and grid[i][j + 2] == ".":
                         key, pt = grid[i][j] + grid[i][j + 1], (i, j + 2)
                         if pt[1] == 2:  # outer left
-                            portal_pairs[key][pt] = outer_key
+                            portal_pairs[key][pt] = out_key
                         else:  # inner right
-                            portal_pairs[key][pt] = inner_key
+                            portal_pairs[key][pt] = in_key
 
                 if j - 2 >= 0:  # v left
                     if grid[i][j - 1].isalpha() and grid[i][j - 2] == ".":
                         key, pt = grid[i][j - 1] + grid[i][j], (i, j - 2)
                         if pt[1] == 28:  # inner left
-                            portal_pairs[key][pt] = inner_key
+                            portal_pairs[key][pt] = in_key
                         else:  # outer right
-                            portal_pairs[key][pt] = outer_key
+                            portal_pairs[key][pt] = out_key
 
     portals = {}
     for k, v in portal_pairs.items():
@@ -102,6 +113,7 @@ def main():
     print_res(day, 1, find_min_path(start, end, grid, portals))
 
     # part 2
+    print_res(day, 2, find_min_path(start, end, grid, portals, recursive=True))
 
 
 if __name__ == '__main__':
