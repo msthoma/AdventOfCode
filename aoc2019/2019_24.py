e3810bb7deda49a33b156cecd7874975ee4349d8
marios <msthoma@yahoo.com>
2020-01-27 15:13:52 +0200

    completed required methods

--- aoc2019/2019_24.py
+++ aoc2019/2019_24.py
@@ -1,6 +1,47 @@
 from utils.utils import day_name, input_fp
 
 
+def biodiversity(grid):
+    score = 0
+    for i, row in enumerate(grid):
+        if i % 2:  # odd rows reversed
+            row = row[::-1]
+        for j, tile in enumerate(row):
+            if tile == "#":
+                score += 2 ** (i * len(row) + j)
+    return score
+
+
+def sim_turn(grid):
+    next_grid = []
+    for i in range(len(grid)):
+        row = []
+        for j in range(len(grid[0])):
+            tile_type = grid[i][j]
+            count = count_adjacent(grid, (i, j))
+            if tile_type == "#" and count == 1:
+                row.append("#")
+            elif tile_type == "." and count in [1, 2]:
+                row.append("#")
+            else:
+                row.append(".")
+        next_grid.append("".join(row))
+    return next_grid
+
+
+def count_adjacent(grid, tile):
+    count = 0
+    for i, j in zip([-1, 0, 1, 0], [0, 1, 0, -1]):
+        i, j = i + tile[0], j + tile[1]
+        if i >= 0 and j >= 0:
+            try:
+                if grid[i][j] == "#":
+                    count += 1
+            except IndexError:
+                pass
+    return count
+
+
 def main():
     day = day_name()
 
@@ -9,7 +50,7 @@ def main():
 
     # part 1
     print(grid)
-
+    print(biodiversity(grid))
     # part 2
 
 
