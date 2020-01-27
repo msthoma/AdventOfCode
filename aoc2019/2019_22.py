982ffc16e1a6024ef52486fb7c555b8dcd438b66
marios <msthoma@yahoo.com>
2020-01-27 12:17:54 +0200

    part 1 answer

--- aoc2019/2019_22.py
+++ aoc2019/2019_22.py
@@ -1,4 +1,23 @@
-from utils.utils import day_name, input_fp
+from utils.utils import day_name, input_fp, print_res
+
+
+def deal_increment(deck, increment):
+    n_deck = [0] * len(deck)
+    position = 0
+    for card in deck:
+        n_deck[position] = card
+        position = (position + increment) % (len(deck))
+    return n_deck
+
+
+def cut_deck(deck, n):
+    n_deck = deck[n:].copy()
+    n_deck.extend(deck[:n])
+    return n_deck
+
+
+def deal_new_stack(deck):
+    return list(reversed(deck))
 
 
 def main():
@@ -7,9 +26,19 @@ def main():
     with open(input_fp(day), "r") as f:
         instructions = (line.strip() for line in f.readlines())
 
-    print(list(instructions))
-
     # part 1
+    deck = [i for i in range(10007)]
+    for instruction in instructions:
+        if "increment" in instruction:
+            increment = int(instruction.lstrip("deal with increment "))
+            deck = deal_increment(deck, increment)
+        elif "cut" in instruction:
+            n = int(instruction.lstrip("cut "))
+            deck = cut_deck(deck, n)
+        elif "new" in instruction:
+            deck = deal_new_stack(deck)
+
+    print_res(day, 1, deck.index(2019))
 
     # part 2
 
