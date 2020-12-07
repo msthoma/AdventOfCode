47262ffcfab3f47757620708c02ff34a30b134ad
marios <msthoma@yahoo.com>
2020-12-07 19:00:32 +0200

    new functions to automatically get input data

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,22 @@
+import functools
 import os
 import sys
+from pathlib import Path
+
+import aocd
+
+
+def data(year: int, day: int):
+    return aocd.get_data(session_id(), year, day)
+
+
+@functools.lru_cache(100)
+def session_id():
+    s_id_file = Path.cwd() / "session_id"
+    assert s_id_file.is_file()
+    with open(s_id_file, "r") as f:
+        s_id = f.read()
+    return s_id
 
 
 def day_name():
