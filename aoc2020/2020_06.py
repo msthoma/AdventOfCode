f12b12b2a75f6af917a6b2f7b062f67d24bdf693
marios <msthoma@yahoo.com>
2020-12-08 18:54:07 +0200

    2020 day 6 answer

---
+++ aoc2020/2020_06.py
@@ -0,0 +1,24 @@
+import re
+
+from utils import utils
+
+
+def main():
+    # import/process day data
+    data = utils.data(2020, 6)
+    answer_a, answer_b = 0, 0
+    # split data at empty lines
+    for entry in re.compile(r"^$", flags=re.MULTILINE).split(data):
+        # part a
+        answer_a += len(set(entry.replace("\n", "")))
+
+        # part b
+        entry = map(set, entry.strip().split("\n"))
+        answer_b += len(set.intersection(*entry))
+
+    print("Answer part a:", answer_a)
+    print("Answer part b:", answer_b)
+
+
+if __name__ == '__main__':
+    main()
