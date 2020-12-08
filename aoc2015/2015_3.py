0842eaf0d151ea004135e3c2c29c4e66636a4904
marios <msthoma@yahoo.com>
2020-12-08 23:13:40 +0200

    2015 day 3 answer

--- aoc2015/2015_3.py
+++ aoc2015/2015_3.py
@@ -1,15 +1,36 @@
-from utils.utils import get_script_file_name, input_fp, res_print
+from utils.utils import get_data, res_print2
+
+
+def calc_new_pos(start_pos: tuple, move: str):
+    y, x = start_pos
+    if move == ">":
+        x += 1
+    elif move == "<":
+        x -= 1
+    elif move == "^":
+        y += 1
+    elif move == "v":
+        y -= 1
+    else:
+        raise ValueError
+    return y, x
 
 
 def main():
-    day = get_script_file_name()
-    with open(input_fp(day), "r") as f:
-        input_data = f.read()
-    print(len(input_data))
+    data = get_data()
 
-    houses = [[0, 0]]
-    for i, move in enumerate(input_data):
-        previous_pos = houses[i]
+    path_a = [(0, 0)]  # (y, x)
+    for move in data:
+        path_a.append(calc_new_pos(path_a[-1], move))
+
+    res_print2(len(set(path_a)), 1)
+
+    path_santa, path_robot = [(0, 0)], [(0, 0)]
+    paths_b = [path_santa, path_robot]
+    for i, move in enumerate(data):
+        paths_b[i % 2].append(calc_new_pos(paths_b[i % 2][-1], move))
+
+    res_print2(len(set(path_santa + path_robot)), 2)
 
 
 if __name__ == '__main__':
