94d47a64405e9d259af6e121155d6c6520624e50
marios <msthoma@yahoo.com>
2020-12-08 22:39:41 +0200

    update to use new util functions

--- aoc2015/2015_2.py
+++ aoc2015/2015_2.py
@@ -1,29 +1,27 @@
 from functools import reduce
 from operator import mul
 
-from utils.utils import get_script_file_name, input_fp, res_print
+from utils.utils import res_print2, get_data
 
 
 def main():
-    day = get_script_file_name()
-    with open(input_fp(day), "r") as f:
-        input_data = f.read()
+    input_data = get_data()
 
-    dims = [sorted([int(d) for d in dim.split(sep="x")]) for dim in input_data.splitlines()]
+    dims = [sorted([int(d) for d in dim.split(sep="x")]) for dim in
+            input_data.splitlines()]
 
     area_needed = 0
     for dim in dims:
         l, w, h = dim
         side_areas = [l * w, w * h, h * l]
         area_needed += 2 * sum(side_areas) + min(side_areas)
-        # 1598415
 
     ribbon_needed = 0
     for dim in dims:
         ribbon_needed += 2 * (dim[0] + dim[1]) + reduce(mul, dim, 1)
 
-    res_print(day, 1, area_needed)
-    res_print(day, 1, ribbon_needed)
+    res_print2(area_needed, 1)
+    res_print2(ribbon_needed, 2)
 
 
 if __name__ == '__main__':
