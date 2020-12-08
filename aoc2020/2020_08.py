e8f69479a78f3b5ffc6ae905e509ab7de7526180
marios <msthoma@yahoo.com>
2020-12-08 20:45:49 +0200

    2020 day 8 part b answer

--- aoc2020/2020_08.py
+++ aoc2020/2020_08.py
@@ -1,12 +1,15 @@
 from utils import utils
 
 
-def run_code(code: list):
-    accumulator, current, executed = 0, 0, set()
+def run_code(code: list, part: str = "a"):
+    accumulator, current, executed, code_len = 0, 0, set(), len(code)
 
-    while True:
+    while current < code_len:
         if current in executed:
-            break
+            if part == "a":
+                return accumulator
+            else:
+                return None
 
         executed.add(current)
         instr, val = code[current]
@@ -26,8 +29,19 @@ def main():
     data = [line.split(" ") for line in utils.data(2020, 8).splitlines()]
     code = [[i, int(j)] for i, j in data]
 
-    print("Answer part a:", run_code(code))
-    print("Answer part b:", 0)
+    # part a
+    print("Answer part a:", run_code(code, part="a"))
+
+    # part b
+    for i, (instr, val) in enumerate(code):
+        if instr == "acc":
+            continue
+        # swap jmp <-> nop, generate new code
+        new_instr = "jmp" if instr == "nop" else "nop"
+        new_code = code[:i] + [[new_instr, val]] + code[i + 1:]
+        # run new code, print answer if it's not None
+        if (res := run_code(new_code, part="b")) is not None:
+            print("Answer part b:", res)
 
 
 if __name__ == '__main__':
