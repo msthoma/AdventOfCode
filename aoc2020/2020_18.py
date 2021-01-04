f783f19d076ed790f78dd5092345558d69d35d40
marios <msthoma@yahoo.com>
2021-01-04 19:14:07 +0200

    2020 day 18 part b answer

--- aoc2020/2020_18.py
+++ aoc2020/2020_18.py
@@ -3,7 +3,7 @@ import re
 from utils.utils import get_data, res_print2
 
 
-def do_strange_math_a(formula):
+def do_math_part_a(formula):
     if type(formula) is re.Match:
         formula = formula.group(0)
 
@@ -11,7 +11,7 @@ def do_strange_math_a(formula):
 
     first = re.compile(r"^\d+? [*|+] \d+")
 
-    while any(p in formula for p in ["*", "+"]):
+    while any(o in formula for o in ["*", "+"]):
         start = first.match(formula)
         if not start:
             break
@@ -23,9 +23,29 @@ def do_strange_math_a(formula):
     return formula
 
 
-def main():
-    data = get_data().splitlines()
+def do_addition(formula: re.Match):
+    if type(formula) is re.Match:
+        formula = formula.group(0)
+    assert "+" in formula
+    return str(eval(formula))
 
+
+def do_math_part_b(formula):
+    if type(formula) is re.Match:
+        formula = formula.group(0)
+
+    formula = formula.strip("()")
+
+    addition = re.compile(r"\d+? \+ \d+")
+
+    while "+" in formula:
+        formula = re.sub(addition, repl=do_addition, string=formula,
+                         count=1)
+    formula = str(eval(formula))
+    return formula
+
+
+def solve_with_rules(rules, data: list):
     # pattern for substrings in parentheses but do not contain inner parentheses
     in_parentheses = re.compile(r"\([^(]+?\)")
 
@@ -34,19 +54,21 @@ def main():
         for line in data:
             if type(line) is not int:
                 if "(" in str(line):
-                    line = re.sub(in_parentheses, repl=do_strange_math_a,
-                                  string=line)
+                    line = re.sub(in_parentheses, repl=rules, string=line)
                 else:
-                    line = int(do_strange_math_a(line))
+                    line = int(rules(line))
             new_data.append(line)
         data = new_data
 
         if all(map(lambda x: type(x) is int, data)):
-            answer_a = sum(data)
-            break
+            return sum(data)
 
-    res_print2(answer_a, 1)
-    res_print2(2, 2)
+
+def main():
+    data = get_data().splitlines()
+
+    res_print2(solve_with_rules(do_math_part_a, data.copy()), 1)
+    res_print2(solve_with_rules(do_math_part_b, data.copy()), 2)
 
 
 if __name__ == '__main__':
