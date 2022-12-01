0108c3b6a4cf6d5c79674f74f95256b84729b950
marios <msthoma@yahoo.com>
2022-12-01 20:13:20 +0200

    Solution day 1 2022

---
+++ aoc2022/2022_01.jl
@@ -0,0 +1,5 @@
+open("day01_input.txt", "r") do f
+    data = sort([sum(parse.(Int, split(line))) for line in split(read(f, String), "\n\n")], rev=true)
+    println("Day 1 - part 1: ", max(data...))
+    println("Day 1 - part 2: ", sum(data[1:3]))
+end
