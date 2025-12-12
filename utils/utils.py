aab0a1706b6d74378434cf92900561c62f5837dd
marios <msthoma@yahoo.com>
2025-12-12 09:36:28 +0200

    Switch from poetry to uv

--- utils/utils.py
+++ utils/utils.py
@@ -13,10 +13,13 @@ def get_data(year: int = None, day: int = None):
     return aocd.get_data(get_session_id(), year=year, day=day)
 
 
-@functools.lru_cache(100)
+@functools.lru_cache
 def get_session_id():
+    # follow these steps to get session id:
+    # https://github.com/wimglenn/advent-of-code-wim/issues/1
+    # put it in a file called "session_id" in AdventOfCode folder, remove session=
     s_id_file = Path.cwd().parent / "session_id"
-    assert s_id_file.is_file()
+    assert s_id_file.is_file(), s_id_file
     with open(s_id_file, "r") as f:
         s_id = f.read()
     return s_id
@@ -27,7 +30,7 @@ def get_year_day():
 
 
 def get_script_file_name():
-    return os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    return Path(sys.argv[0]).stem
 
 
 def input_fp(day):
@@ -39,5 +42,9 @@ def res_print(day_name, part, res):
 
 
 def res_print2(res, part: int, start_time: float = None):
-    total_time = f" (total time: {round(time.time() - start_time, 4)}s)" if start_time is not None else ""
+    total_time = (
+        f" (total time: {round(time.time() - start_time, 4)}s)"
+        if start_time is not None
+        else ""
+    )
     print(f"{get_script_file_name()} part {part} answer: {res}{total_time}", flush=True)
